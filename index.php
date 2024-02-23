6c4fb20acff3ee6c7a245d7f6cc396c775512f39
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2024-02-23 09:56:59 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+		<link rel="stylesheet" type="text/css" href="estilos.css">
+
+	<meta charset="utf-8">
+	<title>Ricardo</title>
+
+</head>
+<body>
+	<div id="logo">
+	<img src="imagens/logoric.jpg">
+</div>
+<div id="menuecasa">
+	
+	<?php
+	
+	include 'menuric.html';
+	?>
+	<div id="inicio">
+	<a href="Ricardo.php"><img src="imagens/inicio.png"></a>
+</div>
+</div>
+<div id="capas">	
+<div id="capafilmes">
+	<a href="Filmesric.php"><img src="imagens/Filmes.png"></a>
+</div>
+<div id="capalivros">
+	<a href="Livrosric.php"><img src="imagens/capalivros.png"></a>
+</div>
+<div id="capamusicas">
+	<a href="Musicasric.php"><img src="imagens/capamusicas.png"></a>
+</div>
+<div id="caparadios">
+	<a href="Radiosric.php"><img src="imagens/caparadios.png"></a>
+</div>
+<div id="capavideos">
+	<a href="Videosric.php"><img src="imagens/capavideos.png"></a>
+</div>
+
+
+
+</body>
+</html>

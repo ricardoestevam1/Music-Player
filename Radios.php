ed8d9bfe0b527e944108ffd2c3e567617eaa4042
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2024-02-23 10:48:04 -0300

    Add files via upload

---
+++ Radios.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<meta charset="utf-8">
+
+	<title>Rádios</title>
+</head>
+<body>
+		<div id="logoradios">
+	<img src="imagens/logoradios.jpg">
+</div>
+<div id="menuecasaradios">
+	
+	<?php
+	
+	include 'menuradios.php';
+	?>
+	<div id="inicio">
+	<a href="Cleusa.php"><img src="imagens/inicio.png"></a>
+</div>
+</div>
+<div id="meioradios">
+
+<div id="FMCultura">
+<a href='http://streaming.procergs.com.br/fmcultura/'><img src="imagens/FMCultura.jpg"></br>FM Cultura 107.7 - Porto Alegre</a>
+</div>
+<div id="Imembui">
+<a href='https://radioimembui.com.br/#/'><img src="imagens/Imembui.jpg"></br>Imembuí - A Rádio da gente daqui</a>
+</div>
+<div id="NativaFM">
+<a href='https://radiosaovivo.net/nativa-santa-maria/'><img src="imagens/NativaFM.jpg"></br>Nativa  FM - Santa Maria</a>
+</div>
+
+</div>
+
+
+</body>
+</html>

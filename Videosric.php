91feab774560ec9e546d04787659a234cfdf35d0
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2024-03-06 21:41:31 -0300

    Add files via upload

---
+++ Videosric.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<meta charset="utf-8">
+
+	<title>Videos</title>
+</head>
+<body>
+		<div id="logovideos">
+	<img src="imagens/logovideos.png">
+</div>
+<div id="menuecasavideos">
+	
+	<?php
+	
+	include 'menuvideosric.php';
+	?>
+	<div id="inicio">
+	<a href="Ricardo.php"><img src="imagens/inicio.png"></a>
+</div>
+</div>
+<div id="meiovideos">
+
+<div id="Teoriasdapersonalidade">
+<a href='Teoriasdapersonalidade.php'><img src="imagens/Psicologia.png"></br>Teorias da Personalidade</a>
+</div>
+
+</div>
+
+
+</body>
+</html>

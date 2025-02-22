9f93bcc73192db0d79b504e77c89d47e1875ef3b
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-02-22 17:17:08 -0300

    Add files via upload

---
+++ music-list2.js
@@ -0,0 +1,72 @@
+let allMusic = [
+   
+    {
+        name: "2. Tridimensional",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "2.Tridimensional"
+    },
+ {
+        name: "3. Que Bom Ver",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "3.Que_Bom_Ver"
+    },
+ {
+        name: "4. Iluminados",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "4.Iluminados"
+    },
+     {
+        name: "5. Canção do Pescador",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "5.Canção_do_Pescador"
+    },
+
+ {
+        name: "6. Flor da Vida",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "6.Flor_da_Vida"
+    },
+
+ {
+        name: "7. A Luz do Amanhecer",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "7.A_Luz_do_Amanhecer"
+    },
+
+ {
+        name: "8. A Chegada da Noite",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "8.A_Chegada_da_Noite"
+    },
+
+ {
+        name: "9. Um Rosto na Madrugada",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "9.Um_Rosto_Na_Madrugada"
+    },
+
+ {
+        name: "10. Memórias de Um Cantador",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "10.Memórias_de_um_Cantador"
+    },
+
+
+     {
+        name: "1. Nossa Música",
+        artist: "(2021) Tridimensional",
+        img: "Capa_tridimensional",
+        src: "1.Nossa_Música"
+    },
+ 
+    
+]

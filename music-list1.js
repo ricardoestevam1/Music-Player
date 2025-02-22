94c414d9af76338f1f567355883427aa82dd0481
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-02-22 10:33:27 -0300

    Add files via upload

---
+++ music-list1.js
@@ -0,0 +1,72 @@
+let allMusic = [
+   
+    {
+        name: "2. Dominante",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "2. Dominante"
+    },
+ {
+        name: "3. Com Minha Voz",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "3.Com_Minha_Voz"
+    },
+ {
+        name: "4. Noite Magia",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "4.Noite_Magia"
+    },
+     {
+        name: "5. Papagaio Pandorga",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "5.Papagaio_Pandorga"
+    },
+
+ {
+        name: "6. Jade Nova",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "6.Jade_Nova"
+    },
+
+ {
+        name: "7. Literatura Brasileira",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "7.Literatura_Brasileira"
+    },
+
+ {
+        name: "8. Mirar Felino",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "8.Mirar_Felino"
+    },
+
+ {
+        name: "9. Lady na Rede",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "9.Lady_Na_Rede"
+    },
+
+ {
+        name: "10. Tito Vai à Praia Mole",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "10.Tito_Vai_À_Praia_Mole"
+    },
+
+
+     {
+        name: "1. Tempo ao Tempo",
+        artist: "(1997) Tempo ao Tempo",
+        img: "Capa_tempoaotempo",
+        src: "1.Tempo_Ao _tempo"
+    },
+ 
+    
+]

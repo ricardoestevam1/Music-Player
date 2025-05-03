43100adb08e0499ab3e4b29938cd1e307fb4e4c0
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 01:36:19 -0300

    Add files via upload

---
+++ javascript/music-list4.js
@@ -0,0 +1,78 @@
+let allMusic = [
+   
+    {
+        name: "02. Better Make It Through Today",
+          artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "02. Better Make It Through Today"
+    },
+ {
+        name: "03. Willie And The Hand Jive",
+         artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "03. Willie And The Hand Jive"
+    },
+ {
+        name: "04. Get Ready",
+       artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "04. Get Ready"
+    },
+     {
+        name: "05. I Shot The Sheriff",
+       artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "05. I Shot The Sheriff"
+    },
+
+ {
+        name: "06. I Can't Hold Out",
+         artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "06. I Can't Hold Out"
+    },
+
+ {
+        name: "07. Please Be With Me",
+       artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "07. Please Be With Me"
+    },
+
+ {
+        name: "08. Let It Grow",
+        artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "08. Let It Grow"
+    },
+
+ {
+        name: "09. Steady Rollin' Man",
+          artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "09. Steady Rollin' Man"
+    },
+
+ {
+        name: "10. Mainline Florida",
+         artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "10. Mainline Florida"
+    },
+    {
+        name: "11. Give Me Strength",
+        artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "11. Give Me Strength"
+    },
+
+
+     {
+        name: "01. Motherless Children",
+        artist: "(1974) 461 Ocean Boulevard",
+        img: "Capa_Eric_Clapton1974",
+        src: "01. Motherless Children"
+    },
+ 
+    
+]

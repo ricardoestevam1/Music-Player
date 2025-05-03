3f900f06b9dfacd98915ec37885013deaaa3612f
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 02:20:13 -0300

    Add files via upload

---
+++ javascript/music-list5.js
@@ -0,0 +1,73 @@
+let allMusic = [
+   
+    {
+        name: "02. Swing Low Sweet Chariot",
+         artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "02. Swing Low Sweet Chariot"
+    },
+ {
+        name: "03. Little Rachel",
+     artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "03. Little Rachel"
+    },
+ {
+        name: "04. Don't Blame Me",
+        artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "04. Don't Blame Me"
+    },
+     {
+        name: "05. The Sky Is Crying",
+      artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "05. The Sky Is Crying"
+    },
+
+ {
+        name: "06. Singin' The Blues",
+         artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "06. Singin' The Blues"
+    },
+
+ {
+        name: "07. Better Make It Through Today",
+       artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "07. Better Make It Through Today"
+    },
+
+ {
+        name: "08. Pretty Blue Eyes",
+         artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "08. Pretty Blue Eyes"
+    },
+
+ {
+        name: "09. High",
+          artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "09. High"
+    },
+
+ {
+        name: "10. Opposites",
+          artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "10. Opposites"
+    },
+   
+
+
+     {
+        name: "01. We've Been Told (Jesus Coming Soon)",
+        artist: "(1975) There's One in Every Crowd",
+        img: "Capa_Eric_Clapton1975",
+        src: "01. We've Been Told (Jesus Coming Soon)"
+    },
+ 
+    
+]

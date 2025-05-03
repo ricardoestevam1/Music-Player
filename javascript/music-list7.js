b17aac7437eb49c2feeabd70f8e133ac2049edd3
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 04:09:36 -0300

    Update music-list7.js

--- javascript/music-list7.js
+++ javascript/music-list7.js
@@ -3,53 +3,53 @@ let allMusic = [
     {
         name: "02. Wonderful Tonight",
      artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+        img: "Capa_Eric_Clapton1977",
         src: "02. Wonderful Tonight"
     },
  {
         name: "03. Lay Down Sally",
  artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+        img: "Capa_Eric_Clapton1977",
         src: "03. Lay Down Sally"
     },
  {
         name: "04. Next Time You See Her",
        artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+           img: "Capa_Eric_Clapton1977",
         src: "04. Next Time You See Her"
     },
      {
         name: "05. We're All The Way",
          artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+            img: "Capa_Eric_Clapton1977",
         src: "05. We're All The Way"
     },
 
  {
         name: "06. The Core",
             artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+      img: "Capa_Eric_Clapton1977",
         src: "06. The Core"
     },
 
  {
         name: "07. May You Never",
      artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+            img: "Capa_Eric_Clapton1977",
         src: "07. May You Never"
     },
 
  {
         name: "08. Mean Old Frisco",
          artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+     img: "Capa_Eric_Clapton1977",
         src: "08. Mean Old Frisco"
     },
 
  {
         name: "09. Peaches And Diesel",
          artist: "(1977) Slowhand",
-        img: "Capa_Eric_Clapton1977,
+      img: "Capa_Eric_Clapton1977",
         src: "09. Peaches And Diesel"
     },
 

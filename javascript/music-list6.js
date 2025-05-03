e4e64841ccff10acec81e067957db41d06b7ff27
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 02:54:11 -0300

    Add files via upload

---
+++ javascript/music-list6.js
@@ -0,0 +1,80 @@
+let allMusic = [
+   
+    {
+        name: "02. Carnival",
+       artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "02. Carnival"
+    },
+ {
+        name: "03. Sign Language",
+       artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "03. Sign Language"
+    },
+ {
+        name: "04. County Jail Blues",
+           artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "04. County Jail Blues"
+    },
+     {
+        name: "05. All Our Past Times",
+          artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "05. All Our Past Times"
+    },
+
+ {
+        name: "06. Hello Old Friend",
+             artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "06. Hello Old Friend"
+    },
+
+ {
+        name: "07. Double Trouble",
+     artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "07. Double Trouble"
+    },
+
+ {
+        name: "08. Innocent Times",
+           artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "08. Innocent Times"
+    },
+
+ {
+        name: "09. Hungry",
+           artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "09. Hungry"
+    },
+
+ {
+        name: "10. Opposites",
+            artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "10. Opposites"
+    },
+   
+    {
+        name: "11. Last Night",
+        artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "11. Last Night"
+    },
+   
+
+
+     {
+        name: "01. Beautiful Thing",
+        artist: "(1976) No Reason to Cry",
+        img: "Capa_Eric_Clapton1976",
+        src: "01. Beautiful Thing"
+    },
+ 
+    
+]

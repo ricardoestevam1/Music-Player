2d0299ef2eac33e5c820a88b697e00db6f520bed
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 00:03:33 -0300

    Add files via upload

---
+++ javascript/music-list3.js
@@ -0,0 +1,78 @@
+let allMusic = [
+   
+    {
+        name: "2. Bad Boy",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "02.Bad_Boy"
+    },
+ {
+        name: "03. Lonesome And A Long Way From Home",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "03. Lonesome And A Long Way From Home"
+    },
+ {
+        name: "04. After Midnight",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "04. After Midnight"
+    },
+     {
+        name: "05. Easy Now",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "05. Easy Now"
+    },
+
+ {
+        name: "06. Blues Power",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "06. Blues Power"
+    },
+
+ {
+        name: "07. Bottle Of Red Wine",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "07. Bottle Of Red Wine"
+    },
+
+ {
+        name: "08. Lovin' You Lovin' Me",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "08. Lovin' You Lovin' Me"
+    },
+
+ {
+        name: "09. Told You For The Last Time",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "09. Told You For The Last Time"
+    },
+
+ {
+        name: "10. Don't Know Why",
+        artist: "(1970) Eric Clapton,
+        img: "Capa_Eric_Clapton1970",
+        src: "10. Don't Know Why"
+    },
+    {
+        name: "11. Let It Rain",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "11. Let It Rain"
+    },
+
+
+     {
+        name: "1. Slunky",
+        artist: "(1970) Eric Clapton",
+        img: "Capa_Eric_Clapton1970",
+        src: "01.Slunky"
+    },
+ 
+    
+]

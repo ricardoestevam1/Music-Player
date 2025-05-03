e9fba57d2c9e31e876505f52f36f88714a4d8a50
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 00:41:48 -0300

    Update music-list3.js

--- javascript/music-list3.js
+++ javascript/music-list3.js
@@ -10,54 +10,54 @@ let allMusic = [
         name: "03. Lonesome And A Long Way From Home",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "03. Lonesome And A Long Way From Home"
+        src: "03.Lonesome_And_A_Long_Way_From_Home"
     },
  {
         name: "04. After Midnight",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "04. After Midnight"
+        src: "04.After_Midnight"
     },
      {
         name: "05. Easy Now",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "05. Easy Now"
+        src: "05.Easy_Now"
     },
 
  {
         name: "06. Blues Power",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "06. Blues Power"
+        src: "06.Blues_Power"
     },
 
  {
         name: "07. Bottle Of Red Wine",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "07. Bottle Of Red Wine"
+        src: "07.Bottle_Of_Red_Wine"
     },
 
  {
         name: "08. Lovin' You Lovin' Me",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "08. Lovin' You Lovin' Me"
+        src: "08.Lovin'_You_Lovin'_Me"
     },
 
  {
         name: "09. Told You For The Last Time",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "09. Told You For The Last Time"
+        src: "09.Told_You_For_The_Last_Time"
     },
    {
         
         name: "10. Don't Know Why",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "10. Don't Know Why"
+        src: "10.Don't_Know_Why"
     },
 
      {
@@ -65,7 +65,7 @@ let allMusic = [
         name: "11. Let It Rain",
         artist: "(1970) Eric Clapton",
         img: "Capa_Eric_Clapton1970",
-        src: "11. Let It Rain"
+        src: "11.Let_It_Rain"
     },
 
 

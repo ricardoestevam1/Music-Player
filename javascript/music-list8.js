9c7c80223aea2a519ebe7929f6437d58f1e63447
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 04:23:16 -0300

    Update music-list8.js

--- javascript/music-list8.js
+++ javascript/music-list8.js
@@ -3,60 +3,60 @@ let allMusic = [
     {
         name: "02. Watch Out For Lucy",
           artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+        img: "Capa_Eric_Clapton1978",
         src: "02. Watch Out For Lucy"
     },
  {
         name: "03. I'll Make Love To You Anytime",
         artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+   img: "Capa_Eric_Clapton1978",
         src: "03. I'll Make Love To You Anytime"
     },
  {
         name: "04. Roll It",
              artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+         img: "Capa_Eric_Clapton1978",
         src: "04. Roll It"
     },
      {
         name: "05. Tell Me That You Love Me",
               artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+       img: "Capa_Eric_Clapton1978",
         src: "05. Tell Me That You Love Me"
     },
 
  {
         name: "06. If I Don't Be There By Morning",
                 artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+   img: "Capa_Eric_Clapton1978",
         src: "06. If I Don't Be There By Morning"
     },
 
  {
         name: "07. Early In The Morning",
         artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+         img: "Capa_Eric_Clapton1978",
         src: "07. Early In The Morning"
     },
 
  {
         name: "08. Promises",
              artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+   img: "Capa_Eric_Clapton1978",
         src: "08. Promises"
     },
 
  {
         name: "09. Peaches And Diesel",
                artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+           img: "Capa_Eric_Clapton1978",
         src: "09. Peaches And Diesel"
     },
 
  {
         name: "10. Tulsa Time",
              artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+           img: "Capa_Eric_Clapton1978",
         src: "10. Tulsa Time"
     },
 
@@ -65,7 +65,7 @@ let allMusic = [
      {
         name: "01. Walk Out In The Rain",
         artist: "(1978) Backless ",
-        img: "Capa_Eric_Clapton1978,
+     img: "Capa_Eric_Clapton1978",
         src: "01. Walk Out In The Rain"
     },
  

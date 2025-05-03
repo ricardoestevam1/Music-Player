66f8426a705db96a0e12c1f1c0c7e7fed2ab9e75
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 09:49:01 -0300

    Add files via upload

---
+++ javascript/music-list00.js
@@ -0,0 +1,82 @@
+let allMusic = [
+   
+    {
+        name: "2.Surpresa_Metalica",
+            artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "2.Surpresa_Metalica"
+    },
+ {
+        name: "3.Platina_Verde",
+        artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "3.Platina_Verde"
+    },
+ {
+        name: "4.Brilhar_Sem_Luz",
+               artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "4.Brilhar_Sem_Luz"
+    },
+     {
+        name: "5.A_Flor_da_Vida",
+               artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "5.A_Flor_da_Vida"
+    },
+
+ {
+        name: "6.Salve-Se_Quem_Souber",
+                 artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "6.Salve-Se_Quem_Souber"
+    },
+
+ {
+        name: "7.Garoupa",
+          artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "7.Garoupa"
+    },
+
+ {
+        name: "8.Imagem_das_Pedras",
+          artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "8.Imagem_das_Pedras"
+    },
+
+ {
+        name: "9.So_por_Milagre",
+      artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "9.So_por_Milagre"
+    },
+
+ {
+        name: "10.O_Bom_da_Vida",
+             artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "10.O_Bom_da_Vida"
+    },
+
+ {
+        name: "11.Olhos_de_Fogo",
+        artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "11.Olhos_de_Fogo"
+    },
+
+
+
+     {
+        name: "1. Pimenta",
+
+
+        artist: "(1992) Imagem das Pedras ",    
+        img: "Capa_Imagem_das_Pedras1992  ",
+        src: "1. Pimenta"
+    },
+ 
+    
+]

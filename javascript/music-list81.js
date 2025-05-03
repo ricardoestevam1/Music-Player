3b110cd95cf42c7af7a5284f23b58adc1e681c10
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 18:57:08 -0300

    Add files via upload

---
+++ javascript/music-list81.js
@@ -0,0 +1,69 @@
+let allMusic = [
+   
+    {
+        name: "02. Black Rose",
+                  artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "02. Black Rose"
+    },
+ {
+        name: "03. Blow Wind Blow",
+               artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "03. Blow Wind Blow"
+    },
+ {
+        name: "04. Another Ticket",
+                     artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "04. Another Ticket"
+    },
+     {
+        name: "05. I Can't Stand It",
+                      artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "05. I Can't Stand It"
+    },
+
+ {
+        name: "06. Hold Me Lord",
+                     artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "06. Hold Me Lord"
+    },
+
+ {
+        name: "07. Floating Bridge",
+             artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "07. Floating Bridge"
+    },
+
+ {
+        name: "08. Catch Me If You Can",
+                   artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "08. Catch Me If You Can"
+    },
+
+ {
+        name: "09. Rita Mae",
+              artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "09. Rita Mae"
+    },
+
+
+
+
+     {
+        name: "01. Something Special",
+
+
+        artist: "(1981) Another Ticket",
+        img: "Capa_Eric_Clapton1981",
+        src: "01. Something Special"
+    },
+ 
+    
+]

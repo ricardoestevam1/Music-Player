1ca29b44c7fd8ad73969313abb1d1501d38937eb
ricardoestevam1 <160549816+ricardoestevam1@users.noreply.github.com>
2025-05-03 19:19:48 -0300

    Add files via upload

---
+++ javascript/music-list83.js
@@ -0,0 +1,75 @@
+let allMusic = [
+   
+    {
+        name: "02. The Shape You're In",
+                artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "02. The Shape You're In"
+    },
+ {
+        name: "03. Ain't Going Down",
+                artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "03. Ain't Going Down"
+    },
+ {
+        name: "04. I've Got A Rock N' Roll Heart",
+                   artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "04. I've Got A Rock N' Roll Heart"
+    },
+     {
+        name: "05. Man Overboard",
+                       artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "05. Man Overboard"
+    },
+
+ {
+        name: "06. Pretty Girl",
+                    artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "06. Pretty Girl"
+    },
+
+ {
+        name: "07. Man In Love",
+              artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "07. Man In Love"
+    },
+
+ {
+        name: "08. Crosscut Saw",
+                    artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "08. Crosscut Saw"
+    },
+
+ {
+        name: "09. Slow Down Linda",
+              artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "09. Slow Down Linda"
+    },
+
+ {
+        name: "10. Crazy Country Hop",
+             artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "10. Crazy Country Hop"
+    },
+
+
+
+     {
+        name: "01. Everybody Oughta Make A Chance",
+
+
+        artist: "(1983) Money And Cigarettes",
+        img: "Capa_Eric_Clapton1983",
+        src: "01. Everybody Oughta Make A Chance"
+    },
+ 
+    
+]
